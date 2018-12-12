b3a644340b1f7c121921d555431d3b5d547b4218
wcolts2000 <wcolts2000@gmail.com>
2018-12-12 13:15:49 -0800

    savepoint, form for adding post nearly done, workers outside, not sure if network will go out

--- cool_table_client/src/ForumPost.js
+++ cool_table_client/src/ForumPost.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { URL } from "./App";
 
-export default function ForumPost() {
-  return (
-    <form>
-      <input />
-    </form>
-  );
+// ==============================
+// =====  Styled Component  =====
+// ==============================
+
+const Form = styled.form``;
+
+// ==============================
+// =====      Component     =====
+// ==============================
+
+export default class ForumPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+  }
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  addPost = () => {
+    const userId = 1;
+    axios
+      .post(
+        `${URL}posts`,
+        `{this.state, id: ${userId}, created_at: ${Date.now()}}`
+      )
+      .then(({ data }) => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.addPost}>
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={this.handleInputChange}
+          placeholder="Title..."
+        />
+        <input
+          type="text"
+          value={this.state.body}
+          onChange={this.handleInputChange}
+          placeholder="Your thoughts..."
+        />
+        <button>Drop&nbsp;some&nbsp;knowledge</button>
+      </Form>
+    );
+  }
 }
